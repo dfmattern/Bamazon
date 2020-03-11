f3e301923252f3498a9fb4a33a7a6d3e2b602f08
Dane <56905214+dfmattern@users.noreply.github.com>
2020-03-10 23:11:15 -0600

    built add inventroy and add product functions

--- bamazonManager.js
+++ bamazonManager.js
@@ -101,3 +101,103 @@ function viewLowInventory() {
     connection.end();
   });
 }
+
+function addInventory() {
+  //console.log("add inventory");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "item_id",
+        message: "Please enter the id of the item to update."
+      },
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many would you like to add?"
+      }
+    ])
+    .then(function(input) {
+      console.log(
+        "You have selected: \n    Item id: " +
+          input.item_id +
+          "\n    Additional Quantity: " +
+          input.quantity
+      );
+
+      let item = input.item_id;
+      let addQuantity = input.quantity;
+
+      let query = "SELECT * FROM products WHERE ?";
+
+      connection.query(query, { item_id: item }, function(err, res) {
+        if (err) throw err;
+
+        //console.log("response = " + JSON.stringify(res));
+
+        if (res.length === 0) {
+          console.log("INVALID ITEM: Please select a valid item id.");
+          addInventory();
+        } else {
+          let productInfo = res[0];
+          //console.log(productInfo);
+
+          let query = connection.query ("UPDATE products SET ? WHERE ?", 
+          [{
+            stock_quantity:addQuantity
+          },
+          {
+            item_id:item
+          }
+        ],
+          //console.log(updateProduct);
+          function(err,res){
+            if (err) throw err;
+            console.log("----------------------------------------\m");
+            
+            console.log(res.affectedRows + " product updated\n");
+            connection.end();
+          }
+        
+          )
+        }
+      });
+    });
+}
+
+function addProduct(){
+  inquirer.prompt([{
+    type:"input",
+    name:"product_name",
+    message:"Please enter the new product name."
+  },
+  {
+type:"input",
+name:"department_name",
+message:"Please enter a department for the new product."
+  },
+  {
+    type:"input",
+    name:"price",
+    message:"What is the price per unit?"
+  },
+  {
+    type:"input",
+    name:"stock_quantity",
+    message:"How many items are you stocking?"
+  }
+]).then(function(input){
+  console.log('You are adding New Item: \n    product_name = ' + input.product_name + '\n' +  
+									   '    department_name = ' + input.department_name + '\n' +  
+									   '    price = ' + input.price + '\n' +  
+                     '    stock_quantity = ' + input.stock_quantity);
+                     
+    let query = "INSERT INTO products SET ?";
+
+    connection.query(query, input, function(err, results, fields){
+      if (err) throw err;
+
+      connection.end();s
+    })
+})
+}
